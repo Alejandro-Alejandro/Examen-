8cc5b722dbbe05cb33ebfcbeb1d9ad833d5337f4
Alejandro Alejandro <Alejandroulloa7@outlook.com>
2024-11-28 19:19:12 -0500

    Fin sección 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
+  base: number = 0;
+  altura: number = 0;
+  area: number = 0;
 
-  constructor() {}
-
+  
+  calcularArea() {
+    if (this.base && this.altura) {
+      this.area = this.base * this.altura;
+    } else {
+      alert('Por favor, ingresa ambos lados.');
+    }
+  }
 }
